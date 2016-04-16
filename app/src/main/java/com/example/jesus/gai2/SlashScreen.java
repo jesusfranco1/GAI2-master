a4590644d02172d3f4f8cf3b84b0219e773e38b0
jesusfranco1 <Jesus@56-61-41-155-wireless1x.bu.edu>
2016-04-15 22:23:23 -0400

    Hi
    Hector

---
+++ app/src/main/java/com/example/jesus/gai2/SlashScreen.java
@@ -0,0 +1,41 @@
+package com.example.jesus.gai2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by Jesus on 4/5/16.
+ */
+public class SlashScreen extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(2000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SlashScreen.this,Please.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
