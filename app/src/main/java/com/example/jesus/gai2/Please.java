3372ea6073bb20033aabac79a72468c0f2601103
Jesus <jofranco@bu.edu>
2016-04-23 21:35:10 -0400

    made webview access on please

--- app/src/main/java/com/example/jesus/gai2/Please.java
+++ app/src/main/java/com/example/jesus/gai2/Please.java
@@ -1,9 +1,6 @@
 package com.example.jesus.gai2;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,6 +9,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
+//import android.webkit.PermissionRequest;
 
 public class Please extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -72,17 +72,55 @@ public class Please extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_twitter) {
+//            Uri uri = Uri.parse("http://www.example.com");
+//            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//            startActivity(intent);
+            WebView twitter = new WebView(this);
+            setContentView(twitter);
+            // Simplest usage: note that an exception will NOT be thrown
+            // if there is an error loading this page (see below).
+            twitter.loadUrl("http://www.example.com");
+
+            // OR, you can also load from an HTML string:
+//            String summary = "<html><body>You scored <b>192</b> points.</body></html>";
+//            twitter.loadData(summary, "text/html", null);
+            // ... although note that there are restrictions on what this HTML can do.
+            // See the JavaDocs for loadData() and loadDataWithBaseURL() for more info.
+
+            // Let's display the progress in the activity title bar, like the
+            // browser app does.
+//            getWindow().requestFeature(Window.FEATURE_PROGRESS);
+//
+//            webview.getSettings().setJavaScriptEnabled(true);
+//
+//            final Activity activity = this;
+//            webview.setWebChromeClient(new WebChromeClient() {
+//                public void onProgressChanged(WebView view, int progress) {
+//                    // Activities and WebViews measure progress with different scales.
+//                    // The progress meter will automatically disappear when we reach 100%
+//                    activity.setProgress(progress * 1000);
+//                }
+//            });
+//            webview.setWebViewClient(new WebViewClient() {
+//                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+//                    Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+//                }
+//            });
+
+            //webview.loadUrl("http://developer.android.com/");
+
+            //Uri uri = Uri.parse("https://twitter.com/?lang=en");
+            //Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+           // startActivity(intent);
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+//        } else if (id == R.id.nav_gallery) {
+//
+//        } else if (id == R.id.nav_manage) {
+//
+//        } else if (id == R.id.nav_share) {
+//
+//        } else if (id == R.id.nav_send) {
 
         }
 
